b405d3fe0bd78c2943a023544d2e995616e48371
kot2d <kot2d@outlook.com>
2023-04-29 23:13:50 +0200

    added commands functionality

--- src/ZookeeperApp.java
+++ src/ZookeeperApp.java
@@ -1,16 +1,15 @@
 package src;
 
 import src.controllers.PetController;
-import src.persistance.AnimalRepository;
-import src.persistance.AnimalRepositoryFile;
-import src.persistance.FileOperationImpl;
+import src.persistance.*;
 import src.views.PetsView;
 
 public class ZookeeperApp {
 
     public static void main(String[] args){
         AnimalRepository repository = new AnimalRepositoryFile(new FileOperationImpl("animals.txt"));
-        PetController controller = new PetController(repository);
+        PetsCommandRepository commandRepository = new PetsCommandRepositoryFile(new FileOperationImpl("commands.txt"));
+        PetController controller = new PetController(repository, commandRepository);
         PetsView view = new PetsView(controller);
         view.start();
     }
--- src/views/PetsView.java
+++ src/views/PetsView.java
@@ -3,6 +3,8 @@ package src.views;
 
 import src.controllers.PetController;
 import src.model.animals.AnimalTypes;
+import src.model.commands.Commands;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -17,44 +19,84 @@ public class PetsView {
         System.out.println("Добро пожаловать в программу Заметки.");
 
         while(true){
-
-            MenuAction action = MenuAction.valueOf(prompt("""
-                    Доступны комнады:\s
-                    CREATE: Добавить питомца
-                    GET: Найти питомца по id
-                    GETALL: Показать всех питомцев
-                    EXIT: Выход\n"""));
+            try {
+                MenuAction action = MenuAction.valueOf(prompt("""
+                        Доступны комнады:\s
+                        CREATE: Добавить питомца
+                        GET: Найти питомца по id
+                        GETALL: Показать всех питомцев
+                        TEACH: Научить питомца комманде
+                        DO: Попросить питомца выполнить комманду
+                        EXIT: Выход\n"""));
 
             switch (action){
                 case CREATE:
                     try{
-                    String petsSelectionString = String.format("Выберете вид питомца: %s", Arrays.asList(AnimalTypes.values()));
-                    String petsType = prompt(petsSelectionString);
+                    String petsSelectionString = String.format("Выберете вид питомца: %s\n", Arrays.asList(AnimalTypes.values()));
+                    AnimalTypes petsType = AnimalTypes.valueOf(prompt(petsSelectionString));
                     String petName = prompt("Введите имя питомца: ");
                     String masterName = prompt("Введите имя хозяина: ");
-                    String note = controller.createPet(petsType, petName, masterName);
-                    printResult(String.format("Заметка создана!\n%s\n", note));
+                    String pet = controller.createPet(petsType, petName, masterName);
+                    printResult(String.format("Питомец создан!\n%s\n", pet));
+                    } catch (IllegalArgumentException e){
+                        throw e;
                     } catch (Exception e){
-                        System.out.println(e.getMessage());
+                        printResult(e.getMessage() + "\n");
                     }
                     break;
                 case GET:
                     try{
-                        String noteId = prompt("Введите id Вашего питомца: ");
-                        printResult(controller.getPetsById(noteId) + "\n");
+                        String petId = prompt("Введите id Вашего питомца: ");
+                        String petData = controller.getPetsById(petId);
+                        String commandData = controller.getCommandsByPetsId(petId);
+                        printResult(String.format("%s\nКоманды: %s\n", petData, commandData));
                     } catch (Exception e){
-                        System.out.println(e.getMessage());
+                        printResult(e.getMessage() + "\n");
                     }
                     break;
                 case GETALL:
                     try{
                         printResult(controller.getAllPets());
                     } catch (Exception e){
-                        System.out.println(e.getMessage());
+                        printResult(e.getMessage() + "\n");
+                    }
+                    break;
+                case TEACH:
+                    try{
+                        String petId = prompt("Введите id Вашего питомца: ");
+                        String commandListString = String.format("Выберете команду для питомца: %s\n",
+                                Arrays.asList(Commands.values())).replace("[","").replaceAll("]", "");
+                        Commands command = Commands.valueOf(prompt(commandListString));
+                        controller.teachPetCommand(petId, command);
+                    } catch (Exception e){
+                        printResult(e.getMessage() + "\n");
+                    }
+                    break;
+                case DO:
+                    try{
+                        String petId = prompt("Введите id Вашего питомца: ");
+                        String petsCommands = controller.getCommandsByPetsId(petId);
+                        if (petsCommands.isBlank()){
+                            printResult("Ваш питомец не знает команд!");
+                        } else {
+                            String commandListString = String.format("Ваш питомец знает комманды: %s\n", controller.getCommandsByPetsId(petId));
+                            Commands command = Commands.valueOf(prompt(commandListString));
+                            controller.runCommand(petId, command);
+                            printResult("\n");
+                        }
+                    } catch (IllegalArgumentException e){
+                        throw e;
+                    } catch (Exception e){
+                        printResult(e.getMessage() + "\n");
                     }
                     break;
                 case EXIT:
                     return;
+            }
+            } catch (IllegalArgumentException e){
+                printResult("Вы ввели неверную комманду, попробуйте еще раз!\n");
+            } catch (Exception e) {
+                printResult(e.getMessage() + "\n");
             }
         }
     }
@@ -68,5 +110,4 @@ public class PetsView {
         System.out.print(message);
         return in.nextLine();
     }
-
 }
