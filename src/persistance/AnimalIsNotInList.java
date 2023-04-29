1d3f4a2bae158a4405e8ea0e499aeb3a296a85e6
kot2d <kot2d@outlook.com>
2023-04-29 23:08:53 +0200

    updated message

--- src/persistance/AnimalIsNotInList.java
+++ src/persistance/AnimalIsNotInList.java
@@ -2,6 +2,6 @@ package src.persistance;
 
 public class AnimalIsNotInList extends Exception{
     public AnimalIsNotInList(String id) {
-        super(String.format("У вас нет животного с таким id '%s'", id));
+        super(String.format("У вас нет питомца с таким id '%s'", id));
     }
 }
